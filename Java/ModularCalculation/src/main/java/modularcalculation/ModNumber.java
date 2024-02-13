c60a54a0fb2a4c4ad18b428aeee154574677f27e
Gert Jan Timmerman <gertt@infosupport.com>
2024-02-13 11:34:06 +0100

    Implemented subtract, shiftleft and shiftright

--- Java/ModularCalculation/src/main/java/modularcalculation/ModNumber.java
+++ Java/ModularCalculation/src/main/java/modularcalculation/ModNumber.java
@@ -1,7 +1,7 @@
 package modularcalculation;
 
 public class ModNumber {
-    public final static int MaxMod = 1024/8;
+    public final static int MaxMod = 1024 / 8;
 
     public final static int LSIZE = 8;
     public final static int ISIZE = 4;
@@ -12,32 +12,29 @@ public class ModNumber {
     public final static int NSIZE = NCOUNT * LSIZE;
     public final static int HexStringLength = NCOUNT * 2;
     public final static int OctalStringLength = (NSIZE % 3 == 0) ? (NSIZE / 3) : (NSIZE / 3 + 1);
-    public final static int DecimalStringLength = (int)(NSIZE * 0.30102999566398119521373889472449) + 1; // log(2)
+    public final static int DecimalStringLength = (int) (NSIZE * 0.30102999566398119521373889472449) + 1; // log(2)
 
-    long [] num = new long[LCOUNT];
+    long[] num = new long[LCOUNT];
 
-    public ModNumber(long n)
-    {
+    public ModNumber(long n) {
         num[0] = n;
     }
-    public ModNumber(long[] num)
-    {
+
+    public ModNumber(long[] num) {
         this.num = num;
     }
-    public ModNumber(ModNumber m)
-    {
+
+    public ModNumber(ModNumber m) {
         num = m.num.clone();
     }
-    public ModNumber(byte[] n)
-    {
+
+    public ModNumber(byte[] n) {
         int lCount = n.length / LSIZE;
         long tmp;
-        for (int i = 0; i < lCount; i++)
-        {
+        for (int i = 0; i < lCount; i++) {
             tmp = 0L;
-            for (int j = 0; j < LSIZE; j++)
-            {
-                tmp |= ((long)n[i * LSIZE + j] << j * 8);
+            for (int j = 0; j < LSIZE; j++) {
+                tmp |= ((long) n[i * LSIZE + j] << j * 8);
             }
             num[i] = tmp;
         }
@@ -45,14 +42,13 @@ public class ModNumber {
         if (lSize == 0)
             return;
         tmp = 0L;
-        for (int j = 0; j < lSize; j++)
-        {
-            tmp |= ((long)n[lCount * LSIZE + j] << j * 8);
+        for (int j = 0; j < lSize; j++) {
+            tmp |= ((long) n[lCount * LSIZE + j] << j * 8);
         }
         num[lCount] = tmp;
     }
-    public ModNumber (long [] num, int byteLen)
-    {
+
+    public ModNumber(long[] num, int byteLen) {
         int lCount = byteLen / LSIZE;
         for (int i = 0; i < lCount; i++)
             this.num[i] = num[i];
@@ -61,47 +57,41 @@ public class ModNumber {
             return;
         long tmp = 0L;
         long mask = 0xff;
-        for (int j = 0; j < lSize; j++)
-        {
+        for (int j = 0; j < lSize; j++) {
             long masked = num[lCount] & mask;
             tmp |= masked;
             mask <<= 8;
         }
         this.num[lCount] = tmp;
     }
-    public ModNumber (int[] arr)
-    {
+
+    public ModNumber(int[] arr) {
         fromIntArray(arr);
     }
-    public int[] toIntArray()
-    {
-        int []result = new int[ICOUNT];
+
+    public int[] toIntArray() {
+        int[] result = new int[ICOUNT];
         long lomask = 0xffffffff;
         long himask = lomask << ISIZE * 8;
-        for (int i = 0; i < LCOUNT; i++)
-        {
-            result[i * 2] = (int)(num[i] & lomask);
-            result[i * 2 + 1] = (int)((num[i] & himask) >> ISIZE * 8);
+        for (int i = 0; i < LCOUNT; i++) {
+            result[i * 2] = (int) (num[i] & lomask);
+            result[i * 2 + 1] = (int) ((num[i] & himask) >> ISIZE * 8);
         }
         return result;
     }
-    public void fromIntArray(int[] arr)
-    {
-        for (int i = 0; i < LCOUNT; i++)
-        {
+
+    public void fromIntArray(int[] arr) {
+        for (int i = 0; i < LCOUNT; i++) {
             num[i] = arr[i * 2];
-            num[i] |= ((long)arr[i * 2 + 1]) << ISIZE * 8;
+            num[i] |= ((long) arr[i * 2 + 1]) << ISIZE * 8;
         }
 
     }
 
     @Override
-    public  boolean equals(Object other)
-    {
-        if (other instanceof ModNumber om)
-        {
-            for (int i = 0; i < om.num.length; i++)
-            {
+    public boolean equals(Object other) {
+        if (other instanceof ModNumber om) {
+            for (int i = 0; i < om.num.length; i++) {
                 if (num[i] != om.num[i])
                     return false;
             }
@@ -109,45 +99,138 @@ public class ModNumber {
         }
         return false;
     }
+
     @Override
-    public int hashCode()
-    {
+    public int hashCode() {
         return num.hashCode();
     }
-     void checkMax(int size)
-    {
+
+    void checkMax(int size) {
         for (int i = size; i < LCOUNT; i++)
             if (num[i] != 0L)
-        throw new IllegalArgumentException("Modulus is too large!");
+                throw new IllegalArgumentException("Modulus is too large!");
     }
 
 
     public static ModNumber subtractScalar(ModNumber l, int r) {
         int[] resInt = l.toIntArray();
         int carry = 0;
-        int ltmp = resInt[0];
+        long ltmp = resInt[0];
+        if (ltmp < 0)
+            ltmp >>>= ModNumber.ISIZE * 8;
         long lr = 0;
-        lr |= r;
+        lr = r;
+        if (r < 0)
+            lr >>>= ModNumber.ISIZE * 8;
         if (ltmp < lr)
             carry = 1;
-        resInt[0] = (int)(ltmp - lr);
-        for (int i = 1; carry > 0 && i < ICOUNT; i++)
-        {
-            if (carry <= resInt[i])
-            {
+        resInt[0] = (int) (ltmp - lr);
+        for (int i = 1; carry > 0 && i < ICOUNT; i++) {
+            if (carry <= resInt[i]) {
                 resInt[i] -= carry;
                 carry = 0;
-            }
-            else
+            } else
                 resInt[i] -= carry;
 
         }
         ModNumber result = new ModNumber(resInt);
         return result;
     }
+
     public static ModNumber subtractAssignScalar(ModNumber l, int r) {
         ModNumber result = subtractScalar(l, r);
         l.num = result.num;
         return l;
+    }
+
+    public static ModNumber subtract(ModNumber l, ModNumber r) {
+        if (l.equals(r))
+            return new ModNumber(0L);              // Optimization
+        int[] resInt = l.toIntArray();
+        int[] lInt = l.toIntArray();
+        int[] rInt = r.toIntArray();
+        int carry = 0;
+        for (int i = 0; i < ICOUNT; i++) {
+            long ltmp = lInt[i];
+            if (ltmp < 0)
+                ltmp >>>= ModNumber.ISIZE * 8;
+            long rtmp = rInt[i];
+            if (rtmp < 0)
+                rtmp >>>= ModNumber.ISIZE * 8;
+            if (ltmp >= carry) {
+                ltmp -= carry;
+                carry = 0;
+            } else
+                ltmp -= carry;
+            if (ltmp < rtmp)
+                carry = 1;
+            resInt[i] = (int) (ltmp - rtmp);
+        }
+        ModNumber result = new ModNumber(resInt);
+        return result;
+    }
+
+    public static ModNumber subtractAssign(ModNumber ml, ModNumber mr) {
+        ModNumber mResult = ModNumber.subtract(ml, mr);
+        ml.num = mResult.num;
+        return mResult;
+    }
+
+
+    public static ModNumber shiftLeft(ModNumber n, int i) {
+        int words = 0;
+        if (i >= ISIZE * 8) {
+            if (i >= NSIZE)
+                return new ModNumber(0L);
+            words = i / (ISIZE * 8);
+            i %= ISIZE * 8;
+        }
+        long lomask = 0xffffffff;
+        long himask = lomask << ISIZE * 8;
+        int[] resInt = new int[ICOUNT];
+        int[] nInt = n.toIntArray();
+        resInt[ICOUNT - 1] = nInt[ICOUNT - words - 1] << i;
+        for (int j = ICOUNT - 2; j >= words; j--) {
+            long tmp = ((long) (nInt[j - words])) << i;
+            resInt[j + 1] |= (int) ((tmp & himask) >>> ISIZE * 8);
+            resInt[j] = (int) (tmp & lomask);
+        }
+        ModNumber mres = new ModNumber(resInt);
+        return mres;
+    }
+
+    public static ModNumber shiftLeftAssign(ModNumber n, int i) {
+        ModNumber mres = shiftLeft(n, i);
+        n.num = mres.num;
+        return mres;
+    }
+
+    public static ModNumber shiftRight(ModNumber n, int i) {
+        int words = 0;
+        if (i >= ISIZE * 8) {
+            if (i >= NSIZE) {
+                return new ModNumber(0L);
+            }
+            words = i / (ISIZE * 8);
+            i %= ISIZE * 8;
+        }
+        long lomask = 0xffffffff;
+        long himask = lomask << ISIZE * 8;
+        int[] resInt = new int[ICOUNT];
+        int[] nInt = n.toIntArray();
+        resInt[0] = nInt[words] >>> i;
+        for (int j = 0; j < ICOUNT - words - 1; j++) {
+            long tmp = ((long) nInt[j + words + 1]) << ((ISIZE * 8) - i);
+            resInt[j] |= (int) (tmp & lomask);
+            resInt[j + 1] = (int) ((tmp & himask) >>> ISIZE * 8);
+        }
+        ModNumber mres = new ModNumber(resInt);
+        return mres;
+    }
+
+    public static ModNumber shiftRightAssign(ModNumber n, int i) {
+        ModNumber mres = shiftRight(n, i);
+        n.num = mres.num;
+        return mres;
     }
 }
--- Java/ModularCalculation/src/test/java/modularcalculation/ModularCalculationTest.java
+++ Java/ModularCalculation/src/test/java/modularcalculation/ModularCalculationTest.java
@@ -1,6 +1,8 @@
 package modularcalculation;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -79,7 +81,467 @@ public class ModularCalculationTest {
             exp[i] = ~0L;
         ModNumber mexp = new ModNumber(exp);
         ModNumber.subtractAssignScalar(ml, r);
-        assertTrue(mexp.equals(ml));
+        assertEquals(mexp, ml);
+    }
+    @Test
+    public void subtractAssignScalarAllFsFromAllFs()
+    {
+        int r = ~0;
+        long [] l = new long[ModNumber.LCOUNT];
+        for (int i = 0; i < ModNumber.LCOUNT; i++)
+            l[i] = ~0L;
+        long[] exp = new long[ModNumber.LCOUNT];
+        exp[0] = (~0L << (ModNumber.ISIZE * 8));
+        for (int i = 1; i < ModNumber.LCOUNT; i++)
+            exp[i] = ~0L;
+        ModNumber ml = new ModNumber(l);
+        ModNumber mexp = new ModNumber(exp);
+        ModNumber.subtractAssignScalar(ml, r);
+        assertEquals(mexp, ml);
+    }
+    @Test
+    public void subtractSimple()
+    {
+        long[] l = new long[ModNumber.LCOUNT];
+        long[] r = new long[ModNumber.LCOUNT];
+        for (int i = 0; i < ModNumber.LCOUNT; i++)
+        {
+            l[i] = 2;
+            r[i] = 1;
+        }
+        ModNumber ml = new ModNumber(l);
+        ModNumber mr = new ModNumber(r);
+        ModNumber mres = ModNumber.subtract(ml, mr);
+        assertEquals(mres, mr);
+    }
+    @Test
+    public void subtractAssignSimple()
+    {
+        long[] l = new long[ModNumber.LCOUNT];
+        long[] r = new long[ModNumber.LCOUNT];
+        for (int i = 0; i < ModNumber.LCOUNT; i++)
+        {
+            l[i] = 2;
+            r[i] = 1;
+        }
+        ModNumber ml = new ModNumber(l);
+        ModNumber mr = new ModNumber(r);
+        ModNumber.subtractAssign(ml, mr);
+        assertEquals(ml, mr);
+
+    }
+    @Test
+    public void subtractWithCarry()
+    {
+        long[] l = new long[ModNumber.LCOUNT];
+        long[] r = new long[ModNumber.LCOUNT];
+        l[0] = 0L;
+        l[1] = 1L;
+        r[0] = 1L;
+        long[] exp = new long[ModNumber.LCOUNT];
+        exp[0] = ~0L;
+        ModNumber ml = new ModNumber(l);
+        ModNumber mr = new ModNumber(r);
+        ModNumber mexp = new ModNumber(exp);
+        ModNumber mres = ModNumber.subtract(ml, mr);
+        assertEquals(mexp, mres);
+
+    }
+    @Test
+    public void subtractAssignWithCarry()
+    {
+        long[] l = new long[ModNumber.LCOUNT];
+        long[] r = new long[ModNumber.LCOUNT];
+        l[0] = 0L;
+        l[1] = 1L;
+        r[0] = 1L;
+        long[] exp = new long[ModNumber.LCOUNT];
+        exp[0] = ~0L;
+        ModNumber ml = new ModNumber(l);
+        ModNumber mr = new ModNumber(r);
+        ModNumber mexp = new ModNumber(exp);
+        ModNumber.subtractAssign(ml, mr);
+        assertEquals(mexp, ml);
+
+    }
+    @Test
+    public void subtractWithCarryAcrossMultipleSections()
+    {
+        long[] l = new long[ModNumber.LCOUNT];
+        long[] r = new long[ModNumber.LCOUNT];
+        l[ModNumber.LCOUNT - 1] = 1L;
+        r[0] = 1L;
+        long[] exp = new long[ModNumber.LCOUNT];
+        for (int i = 0; i < ModNumber.LCOUNT - 1; i++)
+        {
+            exp[i] = ~0L;
+        }
+        ModNumber ml = new ModNumber(l);
+        ModNumber mr = new ModNumber(r);
+        ModNumber mexp = new ModNumber(exp);
+        ModNumber mres = ModNumber.subtract(ml, mr);
+        assertEquals(mexp, mres);
+
+    }
+    @Test
+    public void subtractAssignWithCarryAcrossMultipleSections()
+    {
+        long[] l = new long[ModNumber.LCOUNT];
+        long[] r = new long[ModNumber.LCOUNT];
+        l[ModNumber.LCOUNT - 1] = 1L;
+        r[0] = 1L;
+        long[] exp = new long[ModNumber.LCOUNT];
+        for (int i = 0; i < ModNumber.LCOUNT - 1; i++)
+        {
+            exp[i] = ~0L;
+        }
+        ModNumber ml = new ModNumber(l);
+        ModNumber mr = new ModNumber(r);
+        ModNumber mexp = new ModNumber(exp);
+        ModNumber.subtractAssign(ml, mr);
+        assertEquals(mexp, ml);
+
+    }
+    @Test
+    public void subtractOneFromZero()
+    {
+        ModNumber ml = new ModNumber(0L);
+        ModNumber mr = new ModNumber(1L);
+        long[] exp = new long[ModNumber.LCOUNT];
+        for (int i = 0; i < ModNumber.LCOUNT; i++)
+        {
+            exp[i] = ~0L;
+        }
+        ModNumber mexp = new ModNumber(exp);
+        ModNumber mres = ModNumber.subtract(ml, mr);
+        assertEquals(mexp, mres);
+
+    }
+    @Test
+    public void subtractAssignOneFromZero()
+    {
+        ModNumber ml = new ModNumber(0L);
+        ModNumber mr = new ModNumber(1L);
+        long[] exp = new long[ModNumber.LCOUNT];
+        for (int i = 0; i < ModNumber.LCOUNT; i++)
+        {
+            exp[i] = ~0L;
+        }
+        ModNumber mexp = new ModNumber(exp);
+        ModNumber.subtractAssign(ml, mr);
+        assertEquals(mexp, ml);
+    }
+    @Test
+    public void subtractLeftFsFromZero()
+    {
+        ModNumber ml = new ModNumber(0L);
+        long[] r = new long[ModNumber.LCOUNT];
+        r[ModNumber.LCOUNT - 1] = ~0L;
+        ModNumber mr = new ModNumber(r);
+        long[] exp = new long[ModNumber.LCOUNT];
+        exp[ModNumber.LCOUNT - 1] = 1L;
+        ModNumber mexp = new ModNumber(exp);
+        ModNumber mres = ModNumber.subtract(ml, mr);
+        assertEquals(mexp, mres);
+
+    }
+    @Test
+    public void subtractAssignLeftFsFromZero()
+    {
+        ModNumber ml = new ModNumber(0L);
+        long[] r = new long[ModNumber.LCOUNT];
+        r[ModNumber.LCOUNT - 1] = ~0L;
+        ModNumber mr = new ModNumber(r);
+        long[] exp = new long[ModNumber.LCOUNT];
+        exp[ModNumber.LCOUNT - 1] = 1L;
+        ModNumber mexp = new ModNumber(exp);
+        ModNumber.subtractAssign(ml, mr);
+        assertEquals(mexp, ml);
+
+    }
+    @Test
+    public void subtractAllFsFromZero()
+    {
+        ModNumber ml = new ModNumber(0L);
+        long[] r = new long[ModNumber.LCOUNT];
+        for (int i = 0; i < ModNumber.LCOUNT; i++)
+        {
+            r[i] = ~0L;
+        }
+        ModNumber mr = new ModNumber(r);
+        ModNumber mexp = new ModNumber(1L);
+        ModNumber mres = ModNumber.subtract(ml, mr);
+        assertEquals(mexp, mres);
+
+    }
+    @Test
+    public void subtractAssignAllFsFromZero()
+    {
+        ModNumber ml = new ModNumber(0L);
+        long[] r = new long[ModNumber.LCOUNT];
+        for (int i = 0; i < ModNumber.LCOUNT; i++)
+        {
+            r[i] = ~0L;
+        }
+        ModNumber mr = new ModNumber(r);
+        ModNumber mexp = new ModNumber(1L);
+        ModNumber.subtractAssign(ml, mr);
+        assertEquals(mexp, ml);
+
+    }
+    @Test
+    public void shiftLeftZeroBits()
+    {
+        ModNumber ml = new ModNumber(0x12345678L);
+        ModNumber mexp = new ModNumber(0x12345678L);
+        assertEquals(mexp, ModNumber.shiftLeft(ml,  0));
+
+    }
+    @Test
+    public void shiftLeftOneBit()
+    {
+        ModNumber ml = new ModNumber(0x12345L);
+        ModNumber mexp = new ModNumber(0x2468aL);
+        assertEquals(mexp, ModNumber.shiftLeft(ml,  1));
+
+    }
+    @Test
+    public void shiftLeft32Bits()
+    {
+        ModNumber ml = new ModNumber(0x12345L);
+        ModNumber mexp = new ModNumber(0x1234500000000L);
+        assertEquals(mexp, ModNumber.shiftLeft(ml,  32));
+
+    }
+    @Test
+    public void shiftLeftNSIZEBits()
+    {
+        ModNumber ml = new ModNumber(0x12345L);
+        ModNumber mexp = new ModNumber(0x0L);
+        assertEquals(mexp, ModNumber.shiftLeft(ml,  ModNumber.NSIZE));
+
+    }
+    @Test
+    public void shiftLeftAssignZeroBits()
+    {
+        ModNumber ml = new ModNumber(0x12345678L);
+        ModNumber mexp = new ModNumber(0x12345678L);
+        ModNumber.shiftLeftAssign(ml,  0);
+        assertEquals(mexp, ml);
+
+    }
+    @Test
+    public void shiftLeftAssignOneBit()
+    {
+        ModNumber ml = new ModNumber(0x12345L);
+        ModNumber mexp = new ModNumber(0x2468aL);
+        ModNumber.shiftLeftAssign(ml,  1);
+        assertEquals(mexp, ml);
+
+    }
+    @Test
+    public void shiftLeftAssign32Bits()
+    {
+        ModNumber ml = new ModNumber(0x12345L);
+        ModNumber mexp = new ModNumber(0x1234500000000L);
+        ModNumber.shiftLeftAssign(ml,  32);
+        assertEquals(mexp, ml);
+
+    }
+    @Test
+    public void shiftLeftAssignNSIZEBits()
+    {
+        ModNumber ml = new ModNumber(0x12345L);
+        ModNumber mexp = new ModNumber(0x0L);
+        ModNumber.shiftLeftAssign(ml,  ModNumber.NSIZE);
+        assertEquals(mexp, ml);
+
+    }
+    @Test
+    public void shiftLeft60()
+    {
+        ModNumber ml = new ModNumber(0x0102030405060708L);
+        long[] exp = new long[ModNumber.LCOUNT];
+        exp[0] = 0x8000000000000000L;
+        exp[1] = 0x0010203040506070L;
+        ModNumber mexp = new ModNumber(exp);
+        assertEquals(mexp ,ModNumber.shiftLeft(ml, 60));
+    }
+    @Test
+    public void shiftLeftAssign60()
+    {
+        ModNumber ml = new ModNumber(0x0102030405060708L);
+        long[] exp = new long[ModNumber.LCOUNT];
+        exp[0] = 0x8000000000000000L;
+        exp[1] = 0x0010203040506070L;
+        ModNumber mexp = new ModNumber(exp);
+        ModNumber.shiftLeftAssign(ml, 60);
+        assertEquals(mexp, ml);
+    }
+    @Test
+    public void shiftLeft65()
+    {
+        ModNumber ml = new ModNumber(0x12345L);
+        long[] exp = new long[ModNumber.LCOUNT];
+        exp[1] = 0x2468aL;
+        ModNumber mexp = new ModNumber(exp);
+        assertEquals(mexp ,ModNumber.shiftLeft(ml, 65));
+    }
+    @Test
+    public void shiftLeftAssign65()
+    {
+        ModNumber ml = new ModNumber(0x12345L);
+        long[] exp = new long[ModNumber.LCOUNT];
+        exp[1] = 0x2468aL;
+        ModNumber mexp = new ModNumber(exp);
+        ModNumber.shiftLeftAssign(ml, 65);
+        assertEquals(mexp, ml);
+    }
+    @Test
+    public void shiftLeftNSIZEMinusLSIZETimes8()
+    {
+        ModNumber ml = new ModNumber(0x12345L);
+        long[] exp = new long[ModNumber.LCOUNT];
+        exp[ModNumber.LCOUNT - 1] = 0x12345L;
+        ModNumber mexp = new ModNumber(exp);
+        assertEquals(mexp ,ModNumber.shiftLeft(ml, ModNumber.NSIZE - ModNumber.LSIZE * 8));
+    }
+    @Test
+    public void shiftLeftAssignNSIZEMinusLSIZETimes8()
+    {
+        ModNumber ml = new ModNumber(0x12345L);
+        long[] exp = new long[ModNumber.LCOUNT];
+        exp[ModNumber.LCOUNT - 1] = 0x12345L;
+        ModNumber mexp = new ModNumber(exp);
+        ModNumber.shiftLeftAssign(ml, ModNumber.NSIZE - ModNumber.LSIZE * 8);
+        assertEquals(mexp, ml);
+    }
+    @Test
+    public void shiftRightZeroBits()
+    {
+        ModNumber ml = new ModNumber(0x12345678L);
+        ModNumber mexp = new ModNumber(0x12345678L);
+        assertEquals(mexp, ModNumber.shiftRight(ml,  0));
+
+    }
+    @Test
+    public void shiftRightOneBit()
+    {
+        ModNumber ml = new ModNumber(0x12345L);
+        ModNumber mexp = new ModNumber( 0x091a2L);
+        assertEquals(mexp, ModNumber.shiftRight(ml,  1));
+
+    }
+    @Test
+    public void shiftRight32Bits()
+    {
+        ModNumber ml = new ModNumber(0x1234500000000L);
+        ModNumber mexp = new ModNumber(0x12345L);
+        assertEquals(mexp, ModNumber.shiftRight(ml,  32));
+
+    }
+    @Test
+    public void shiftRightISIZEBytes()
+    {
+        ModNumber ml = new ModNumber(0x12345L);
+        ModNumber mexp = new ModNumber(0x0L);
+        assertEquals(mexp, ModNumber.shiftRight(ml,ModNumber.ISIZE * 8));
+
+    }
+    @Test
+    public void shiftRightAssignZeroBits()
+    {
+        ModNumber ml = new ModNumber(0x12345678L);
+        ModNumber mexp = new ModNumber(0x12345678L);
+        ModNumber.shiftRightAssign(ml,  0);
+        assertEquals(mexp, ml);
+
+    }
+    @Test
+    public void shiftRightAssignOneBit()
+    {
+        ModNumber ml = new ModNumber(0x12345L);
+        ModNumber mexp = new ModNumber(0x091a2L);
+        ModNumber.shiftRightAssign(ml,  1);
+        assertEquals(mexp, ml);
+
+    }
+    @Test
+    public void shiftRightAssign32Bits()
+    {
+        ModNumber ml = new ModNumber(0x1234500000000L);
+        ModNumber mexp = new ModNumber(0x12345L);
+        ModNumber.shiftRightAssign(ml,  32);
+        assertEquals(mexp, ml);
+
+    }
+    @Test
+    public void shiftRightAssignISIZEBytes()
+    {
+        ModNumber ml = new ModNumber(0x12345L);
+        ModNumber mexp = new ModNumber(0x0L);
+        ModNumber.shiftRightAssign(ml,  ModNumber.ISIZE * 8);
+        assertEquals(mexp, ml);
+
+    }
+    @Test
+    public void shiftRight60()
+    {
+        ModNumber mexp = new ModNumber(0x0102030405060708L);
+        long[] l = new long[ModNumber.LCOUNT];
+        l[0] = 0x8000000000000000L;
+        l[1] = 0x0010203040506070L;
+        ModNumber ml = new ModNumber(l);
+        assertEquals(mexp ,ModNumber.shiftRight(ml, 60));
+    }
+    @Test
+    public void shiftRightAssign60()
+    {
+        ModNumber mexp = new ModNumber(0x0102030405060708L);
+        long[] l= new long[ModNumber.LCOUNT];
+        l[0] = 0x8000000000000000L;
+        l[1] = 0x0010203040506070L;
+        ModNumber ml = new ModNumber(l);
+        ModNumber.shiftRightAssign(ml, 60);
+        assertEquals(mexp, ml);
+    }
+    @Test
+    public void shiftRight65()
+    {
+        ModNumber mexp = new ModNumber(0x12345L);
+        long[] l = new long[ModNumber.LCOUNT];
+        l[1] = 0x2468aL;
+        ModNumber ml = new ModNumber(l);
+        assertEquals(mexp ,ModNumber.shiftRight(ml, 65));
+    }
+    @Test
+    public void shiftRightAssign65()
+    {
+        ModNumber mexp = new ModNumber(0x12345L);
+        long[] l = new long[ModNumber.LCOUNT];
+        l[1] = 0x2468aL;
+        ModNumber ml = new ModNumber(l);
+        ModNumber.shiftRightAssign(ml, 65);
+        assertEquals(mexp, ml);
+    }
+    @Test
+    public void shiftRightNSIZEMinusLSIZETimes8()
+    {
+        ModNumber mexp = new ModNumber(0x12345L);
+        long[] l = new long[ModNumber.LCOUNT];
+        l[ModNumber.LCOUNT - 1] = 0x12345L;
+        ModNumber ml = new ModNumber(l);
+        assertEquals(mexp ,ModNumber.shiftRight(ml, ModNumber.NSIZE - ModNumber.LSIZE * 8));
+    }
+    @Test
+    public void shiftRightAssignNSIZEMinusLSIZETimes8()
+    {
+        ModNumber mexp = new ModNumber(0x12345L);
+        long[] l = new long[ModNumber.LCOUNT];
+        l[ModNumber.LCOUNT - 1] = 0x12345L;
+        ModNumber ml = new ModNumber(l);
+        ModNumber.shiftRightAssign(ml, ModNumber.NSIZE - ModNumber.LSIZE * 8);
+        assertEquals(mexp, ml);
     }
 
 }
